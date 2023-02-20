e270e26385799348f870b07f220323657486824c
debora.maciel@techer.com.br <debora.maciel@techer.com.br>
2023-02-20 16:42:47 -0300

    Login page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,31 +4,15 @@ import { HomeComponent } from './pages/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
+import { MovieComponent } from './pages/movie/movie.component';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    component: AppComponent,
-    children: [
-      {
-        path: '',
-        component: HomeComponent,
-      },
-    ]
-  },
-  {
-    path: 'information',
-    component: PersonalComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
-  }
+  { path: 'login', component: LoginComponent },
+  { path: '', component: AppComponent, children:[
+    {path: '', component: HomeComponent},
+    {path: 'information', component: PersonalComponent},
+    { path: 'movie', component: MovieComponent },
+  ] },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { LoginComponent } from './auth/login/login.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 //Components
@@ -9,18 +10,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PersonalComponent } from './pages/personal/personal.component';
 import { NavMenuComponent } from 'src/core/components/nav-menu/nav-menu.component';
+import { MovieComponent } from './pages/movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SideMenuComponent,
+    MovieComponent,
     NavMenuComponent,
     HomeComponent,
     PersonalComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
