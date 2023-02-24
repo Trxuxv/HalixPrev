8676d8e403888796d83bc10832340ca2b281a55c
debora.maciel@techer.com.br <debora.maciel@techer.com.br>
2023-02-24 16:01:57 -0300

    Books.

--- src/app/pages/personal/anotation/data/data.ts
+++ src/app/pages/personal/anotation/data/data.ts
@@ -4,11 +4,44 @@ export const anotation: Anotation[] = [
   {
     id: '1',
     title: 'Anotation (Cristianism and Homosexuality)',
-    content: 'Cristianism and the homosexuality does have'
+    content: 'Me chamo Débora Maciel, tenho vinte e um anos e moro no Brasil. Desde muito nova tenho uma ' +
+      'curiosidade intrinseca na minha conciencia, sempre tive sede de conhecimento, sobre assuntos variados, atualmente,' +
+      'estou escrevendo essa pequena tese sobre o assunto: Cristianismo e a Homossexualidade - a escolha desse assunto foi por ' +
+      'conta do quão intrigada eu fiquei sobre algumas questões de analises pessoais e argumentações sobre esse assunto. ' +
+      'Ocorre que, são dois assuntos vistos estritamente como tabus na sociedade atual e não atual da vivencia humana, sendo ' +
+      'abordada então, por um dos livros mais antigos da humanidade, a Biblia. Um adendo também ao fato de eu querer escrever essa tese' +
+      'tem a ver também com minha vivência pessoal nisso tudo, pessoas próximas a mim que já gesticulamos sobre ou até outras pessoas nas redes que passam pelo mesmo questionamento:'+
+      'a homossexualidade é realmente errada segundo a biblia e ao cristianismo?' +
+      ' Esse será o ponto principal a ser mencionado nesse texto, isso significa que irei reunir todos os pontos e argumentos a fim de me ajudar e ajudar talvez quem possa ler isso a ' +
+      'encontrar uma opinião sobre o assunto.'+
+      'curiosidade intrinseca na minha conciencia, sempre tive sede de conhecimento, sobre assuntos variados, atualmente,' +
+      'estou escrevendo essa pequena tese sobre o assunto: Cristianismo e a Homossexualidade - a escolha desse assunto foi por ' +
+      'conta do quão intrigada eu fiquei sobre algumas questões de analises pessoais e argumentações sobre esse assunto. ' +
+      'Ocorre que, são dois assuntos vistos estritamente como tabus na sociedade atual e não atual da vivencia humana, sendo ' +
+      'abordada então, por um dos livros mais antigos da humanidade, a Biblia. Um adendo também ao fato de eu querer escrever essa tese' +
+      'tem a ver também com minha vivência pessoal nisso tudo, pessoas próximas a mim que já gesticulamos sobre ou até outras pessoas nas redes que passam pelo mesmo questionamento:' +
+      'a homossexualidade é realmente errada segundo a biblia e ao cristianismo?' +
+      ' Esse será o ponto principal a ser mencionado nesse texto, isso significa que irei reunir todos os pontos e argumentos a fim de me ajudar e ajudar talvez quem possa ler isso a ' +
+      'encontrar uma opinião sobre o assunto.'+
+      'curiosidade intrinseca na minha conciencia, sempre tive sede de conhecimento, sobre assuntos variados, atualmente,' +
+      'estou escrevendo essa pequena tese sobre o assunto: Cristianismo e a Homossexualidade - a escolha desse assunto foi por ' +
+      'conta do quão intrigada eu fiquei sobre algumas questões de analises pessoais e argumentações sobre esse assunto. ' +
+      'Ocorre que, são dois assuntos vistos estritamente como tabus na sociedade atual e não atual da vivencia humana, sendo ' +
+      'abordada então, por um dos livros mais antigos da humanidade, a Biblia. Um adendo também ao fato de eu querer escrever essa tese' +
+      'tem a ver também com minha vivência pessoal nisso tudo, pessoas próximas a mim que já gesticulamos sobre ou até outras pessoas nas redes que passam pelo mesmo questionamento:' +
+      'a homossexualidade é realmente errada segundo a biblia e ao cristianismo?' +
+      ' Esse será o ponto principal a ser mencionado nesse texto, isso significa que irei reunir todos os pontos e argumentos a fim de me ajudar e ajudar talvez quem possa ler isso a ' +
+      'encontrar uma opinião sobre o assunto.'+
+      'curiosidade intrinseca na minha conciencia, sempre tive sede de conhecimento, sobre assuntos variados, atualmente,' +
+      'estou escrevendo essa pequena tese sobre o assunto: Cristianismo e a Homossexualidade - a escolha desse assunto foi por ' +
+      'conta do quão intrigada eu fiquei sobre algumas questões de analises pessoais e argumentações sobre esse assunto. ' +
+      'Ocorre que, são dois assuntos vistos estritamente como tabus na sociedade atual e não atual da vivencia humana, sendo ' +
+      'abordada então, por um dos livros mais antigos da humanidade, a Biblia. Um adendo também ao fato de eu querer escrever essa tese' +
+      'tem a ver também com minha vivência pessoal nisso tudo, pessoas próximas.'
   },
   {
     id: '2',
-    title: 'Anotation test',
+    title: 'Veganismo',
     content: ''
   }
 ]
